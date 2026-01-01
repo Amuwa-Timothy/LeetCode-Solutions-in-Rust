3dd5cdfae7cf1dab3c994f065db08974352523ff
Amuwa Timothy <62215164+Amuwa-Timothy@users.noreply.github.com>
2026-01-01 01:26:22 +0100

    Time: 9 ms (36.02%), Space: 3.7 MB (65.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.rs
+++ 0217-contains-duplicate/0217-contains-duplicate.rs
@@ -4,13 +4,12 @@ impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
         let mut seen = HashSet::new();
 
-        for num in nums{
-            if seen.contains(&num){
+        for num in nums {
+            if seen.contains(&num) {
                 return true
-            }else{
+            }else {
                 seen.insert(num);
-            }
+            };
         }
-        return false
-    }
-}
+        false
+}   }
