bbdd90ab8493dd3cbf94669a59e4c5c1a186ef3b
Amuwa Timothy <62215164+Amuwa-Timothy@users.noreply.github.com>
2026-01-03 09:02:17 +0100

    Time: 7 ms (27.67%), Space: 5.1 MB (40.51%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+
+        let mut anagrams: HashMap<String, Vec<String>> = HashMap::new();
+
+        for word in strs {
+            let mut chars: Vec<char> = word.chars().collect();
+            chars.sort();
+            let fingerprint: String = chars.into_iter().collect();
+            anagrams.entry(fingerprint).or_insert(Vec::new()).push(word);
+        }
+        anagrams.into_values().collect()
+        
+    }
+}
